93360274bf2f50d906462f7acd43b9ec015e9b98
nnikolov <nnikolov@twilio.com>
2020-09-02 00:36:41 +0300

    New notice section in footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { LangComponent } from '../lang-component';
+import { GoogleSheetsService } from '../google-sheets.service';
+import { TranslateService } from '@ngx-translate/core';
+import { GoogleSheetsURLs } from '../google-sheets-data';
+import { parseSheetsData } from '../google-sheets-parser';
+
+export interface Notice {
+  text: {}
+}
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.sass']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent extends LangComponent implements OnInit {
 
-  constructor() { }
+  notice: Notice;
+
+  constructor(
+    private sheetsService: GoogleSheetsService,
+    private _translate: TranslateService
+  ) {
+    super(_translate);
+  }
 
   ngOnInit(): void {
+    this.sheetsService.getJsonData(GoogleSheetsURLs.noticeURL).subscribe(
+      data => {
+        this.notice = parseSheetsData<Notice>(data)[0];
+
+        console.log(this.notice);
+      }
+    );
   }
 
 }
--- src/app/shared/google-sheets-data.ts
+++ src/app/shared/google-sheets-data.ts
@@ -5,4 +5,5 @@ export const GoogleSheetsURLs = {
     abroadURL: 'https://spreadsheets.google.com/feeds/cells/1Be1n5G-jnS8q2mE_3X2r11rUPFhXg5_3-3yvWndUbW8/4/public/full?alt=json',
     resourcesURL: 'https://spreadsheets.google.com/feeds/cells/1Be1n5G-jnS8q2mE_3X2r11rUPFhXg5_3-3yvWndUbW8/5/public/full?alt=json',
     initiativeURL: 'https://spreadsheets.google.com/feeds/cells/1Be1n5G-jnS8q2mE_3X2r11rUPFhXg5_3-3yvWndUbW8/6/public/full?alt=json',
+    noticeURL: 'https://spreadsheets.google.com/feeds/cells/1Be1n5G-jnS8q2mE_3X2r11rUPFhXg5_3-3yvWndUbW8/7/public/full?alt=json',
 }
